867fa5dddfe397402209e9142adbcc70419f51cf
Abdulquadri-Mahmud <abdulquadrimahmud06@gmail.com>
2025-03-28 15:10:14 +0100

    change asmade on the vite.config file

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       '/api': {
         target: 'https://vtu-xpwk.onrender.com',
         changeOrigin: true,
-        secure: false
+        secure: true
       }
     }
   }
