fe20a62ac1e0e96c3f927c9ba1f2a2083b13cbc8
Abdulquadri-Mahmud <abdulquadrimahmud06@gmail.com>
2025-03-28 21:21:44 +0100

    changes made

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'https://vtu-xpwk.onrender.com',
-  //       changeOrigin: true,
-  //       secure: false
-  //     }
-  //   }
-  // }
+  server: {
+    historyApiFallback: true, // Ensures React Router works on refresh
+  },
 });
